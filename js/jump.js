ddcfa1ade865a956b8012c0454ee1c6f3957759e
martinloman <martin.loman@gmail.com>
2025-03-28 13:23:36 +0100

    Readded scaling of canvas.

--- js/jump.js
+++ js/jump.js
@@ -1,4 +1,8 @@
 let canvas = document.querySelector("canvas")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+canvas.style.width = "100%"
+canvas.style.height = "100%"
 
 /** @type {CanvasRenderingContext2D} */
 let c = canvas.getContext("2d")
